93cab1e11d67bbb85c33717cf8a75eaa67227a23
ahmetonay <ahmet68onay@gmail.com>
2022-04-04 20:37:35 +0200

    added some unit tests

--- src/test/java/MCPTest.java
+++ src/test/java/MCPTest.java
@@ -1,11 +1,32 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import trafficlight.Observer;
-import trafficlight.Subject;
 import trafficlight.ctrl.TrafficLightCtrl;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MCPTest {
+    TrafficLightCtrl trafficLightCtrl = TrafficLightCtrl.getInstance();
+
+    @Test
+    void stateGreenTest() {
+        String actual = trafficLightCtrl.getGreenState().getColor();
+        String expected = "green";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void stateRedTest() {
+        String actual = trafficLightCtrl.getRedState().getColor();
+        String expected = "red";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void stateYellowTest() {
+        String actual = trafficLightCtrl.getYellowState().getColor();
+        String expected = "yellow";
+        assertEquals(expected,actual);
+    }
 
 }
